8d4c9ef19224d5c5b3193c573e96be3e883ac3bf
Emily <57361805+emilygracekz@users.noreply.github.com>
2020-02-05 19:04:12 +0200

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,22 @@
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+  //hide all tab panels
+  tabPanels.forEach(function(panel) {
+    panel.hidden = true;
+  });
+  //mark all tabs as unselected
+  tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false);
+  });
+  //mark clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  //find the associated tab panel and show it
+  const { id } = event.currentTarget;
+const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener("click", handleTabClick));
